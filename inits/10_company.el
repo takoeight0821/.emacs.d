b21236f521c03cf8046d6840fa70df2191b58656
Kono Yuya <takohati0821@gmail.com>
2016-04-01 16:53:47 +0900

    racerをcompanyで使う

---
+++ inits/10_company.el
@@ -0,0 +1,1 @@
+(need-package 'company)
--- inits/20_rust.el
+++ inits/20_rust.el
@@ -9,6 +9,11 @@
     (setq racer-cmd (expand-file-name "/Users/konoyuya/.cargo/bin/racer"))
     (setq racer-rust-src-path (expand-file-name "/Users/konoyuya/rustc-src-1.7.0/src/"))
     (add-hook 'rust-mode-hook #'racer-mode)
-    (use-package ac-racer
-      :config
-      (add-hook 'racer-mode-hook #'(lambda () (ac-racer-setup))))))
+    ;; (use-package ac-racer
+    ;;   :config
+    ;;   (add-hook 'racer-mode-hook #'(lambda () (ac-racer-setup))))
+    (add-hook 'racer-mode-hook #'eldoc-mode)
+    (use-package company)
+    (add-hook 'racer-mode-hook #'company-mode)
+    (global-set-key (kbd "TAB") #'company-indent-or-complete-common)
+    (setq company-tooltip-align-annotations t)))
