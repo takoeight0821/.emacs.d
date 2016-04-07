b0557900c2ffa3c9f4c9f8d529e024beaef04e7e
Kono Yuya <takohati0821@gmail.com>
2016-04-07 22:05:14 +0900

    ocaml, twittering-modeを追加。schemeにrainbow-delimitersをかける

--- inits/20_ocaml.el
+++ inits/20_ocaml.el
@@ -7,6 +7,7 @@
 ;; Add opam emacs directory to the load-path
 (setq opam-share (substring (shell-command-to-string "opam config var share 2> /dev/null") 0 -1))
 (add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
+
 ;; Load merlin-mode
 (require 'merlin)
 ;; Start merlin on ocaml files
---
+++ inits/20_twittering-mode.el
@@ -0,0 +1,3 @@
+(add-to-list 'load-path (expand-file-name "site-lisp/twittering-mode" *emacs-config-directory*))
+
+(require 'twittering-mode)
