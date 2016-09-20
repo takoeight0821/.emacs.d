11e03c386448da65b210ae4238c4ca906b989197
Yuya Kono <takohati0821@gmail.com>
2016-09-20 16:54:40 +0900

    add elixir

--- inits/20-elixir.el
+++ inits/20-elixir.el
@@ -1,12 +1,17 @@
 (package-bundle 'elixir-mode)
+(package-bundle 'alchemist)
+(package-bundle 'ruby-end)
+
 (use-package elixir-mode
   :config
+  (require 'ruby-mode)
   (add-to-list 'elixir-mode-hook
                (defun auto-activate-ruby-end-mode-for-elixir-mode ()
                  (set (make-variable-buffer-local 'ruby-end-expand-keywords-before-re)
                       "\\(?:^\\|\\s-+\\)\\(?:do\\)")
                  (set (make-variable-buffer-local 'ruby-end-check-statement-modifiers) nil)
                  (ruby-end-mode +1)))
+  (add-to-list 'elixir-mode #'auto-complete-mode)
   (sp-with-modes '(elixir-mode)
     (sp-local-pair "fn" "end"
                    :when '(("SPC" "RET"))
@@ -14,4 +19,5 @@
     (sp-local-pair "do" "end"
                    :when '(("SPC" "RET"))
                    :post-handlers '(sp-ruby-def-post-handler)
-                   :actions '(insert navigate))))
+                   :actions '(insert navigate)))
+  (use-package alchemist))
